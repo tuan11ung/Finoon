ab4b30954dac3cb75ec81a4f4c59a6cb9e7acac6
Zede <vzzederzv@gmail.com>
2025-01-18 16:58:19 +0700

    init Spring boot project

---
+++ src/test/java/com/example/finoon/FinoonApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.example.finoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
