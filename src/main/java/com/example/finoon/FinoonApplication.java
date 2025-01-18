7a995e202a1114385475a0a38c0321bdacef853d
Zede <vzzederzv@gmail.com>
2025-01-18 17:20:15 +0700

    init Spring boot project

---
+++ src/main/java/com/example/finoon/FinoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.finoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finoon/FinoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
